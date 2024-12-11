5095305c23014bf24c396e3e3b28c647f22bc569
Egor Gorbachev <egor.v.gor@gmail.com>
2024-12-11 18:14:36 +0300

    Solve task #2

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,14 @@
-let id = document.getElementById('snippet_id').value
-let btn = document.getElementById('enter_snippet_id')
+document.addEventListener('DOMContentLoaded', () => {
+    let btn = document.getElementById('enter_snippet_id');
 
-btn.addEventListener('click', () => {
-    window.location.href = `/snippet/${id}`
-})
+    btn.addEventListener('click', (event) => {
+        event.preventDefault();
+        let id = document.getElementById('snippet_id').value;
+        if (id) {
+            window.location.href = `/snippets/${id}`;
+        } else {
+            alert('Please enter a valid ID');
+        }
+    });
+});
+
